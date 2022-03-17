064a20086b6e9a90ab553218c5b922f2317960f8
ZachTurnPike <zachturnpike@gmail.com>
2022-03-17 19:54:29 -0400

    started adding class functions

--- src/crawler/worker/worker.hpp
+++ src/crawler/worker/worker.hpp
@@ -1,8 +1,29 @@
 #ifndef __WORKER_H
 #define __WORKER_H
 
-class WebcrawlerWorker {
+#include <thread>
+#include <functional>
 
+class WebcrawlerWorker {
+    public:
+        enum class WorkerState {
+            WAITING_WORK,
+            FETCHING,
+            PARSING
+        };
+    private:
+        WorkerState workerState = WorkerState::WAITING_WORK;
+        std::thread workerThread;
+        std::string fetchUrl;
+
+        void workerThread();
+
+    public:
+        WebcrawlerWorker();
+        ~WebcrawlerWorker();
+
+        // Assign a asyncronous crawler work
+        void assignWork(std::string url, std::function<void(std::vector<std::string>)> notifier);
 };
 
 #endif
